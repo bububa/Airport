9be4a1b44a44d0d1e3cbe8fdb6652ebe7ab8bb5b
bububa <prof.syd.xu@gmail.com>
2010-04-24 18:49:00 +0800

    make feedCrawler to support to crawl full rss

--- include/airport/FeedCrawler.h
+++ include/airport/FeedCrawler.h
@@ -14,6 +14,8 @@
 #include <vector>
 #include <ticpp/ticpp.h>
 #include <ticpp/tinystr.h>
+#include <tidy/tidy.h>
+#include <tidy/buffio.h>
 
 namespace airport
 {
@@ -26,21 +28,29 @@ namespace airport
         RDF
     } FeedType;
     
+    typedef std::pair<TidyNode, int> nod_t;
+    
     class FeedCrawler
     {
         airport::HttpClient httpClient;
         bool saveInMongo;
+        bool fullRss;
+        bool enableUpdate;
         int results;
+        airport::nod_t bestMatchNode( TidyDoc tdoc, TidyNode tnod, std::string &request );
     public:
         FeedCrawler();
         ~FeedCrawler();
         void set_save_in_mongo(bool save);
+        void set_full_rss(bool full);
+        void set_enable_update(bool update);
         void set_http_client(airport::HttpClient &hc);
         std::pair<airport::Feed, std::vector<airport::FeedEntry> > get(const char *url);
         std::pair<airport::Feed, std::vector<airport::FeedEntry> > get(std::string &url);
         std::pair<airport::Feed, std::vector<airport::FeedEntry> > parse(airport::HttpResponse &httpResponse);
         std::pair<airport::Feed, std::vector<airport::FeedEntry> > parseRSS2(ticpp::Document &tdoc, airport::HttpResponse &httpResponse, const airport::FeedType feedType);
         airport::FeedEntry rss2Entry(const ticpp::Element *item, std::string &feedLink, std::string &webLink, std::string &author);
+        std::pair<std::string, std::string> extractContent(std::string &html, std::string &title, std::string &content);
         airport::FeedType detectType(ticpp::Document &tdoc);
         void save(airport::Feed &feed, std::vector<airport::FeedEntry> &entries);
         
